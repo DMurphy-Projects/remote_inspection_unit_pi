9608c0f9ca62f7d9b8b313d086c5a59710bd031a
Deansenburg <dmurphy1994@hotmail.co.uk>
2015-03-01 13:21:19 +0000

    change to angle wrapping

--- lword to dec.py
+++ lword to dec.py
@@ -4,23 +4,24 @@ def lwordToDec(num):
     x = num[3] + (256*num[2]) + (65536*num[1]) + (16777216*num[0])
     return x
 
+def normaliseAngle(angle):
+    angle = angle % (2*m.pi)
+    angle = (angle + (2*m.pi)) % (2*m.pi)
+    return angle
+
 def getAngle(pos, diameter):
-    angle = pos/(diameter*m.pi)
-    angle *= 100
-    angle *= m.pi/50
-    while angle > 2*m.pi:
-        angle -= 2*m.pi
+    angle = pos/(diameter*m.pi) * 100 # percentage of perimeter compared to distance moved
+    angle *= m.pi/50 #convert percentage of perimter to radian angle
+    angle = normaliseAngle(angle)
     angle *=180/m.pi #convert to degrees
     return angle
 
 diameter = 100
+degreeMove = (m.pi * diameter)/360
 leftMotor = lwordToDec([0, 0, 0, 0])
 rightMotor = lwordToDec([0, 0, 0, 0])
 
-for x in range(10):
-    if leftMotor > rightMotor:
-        dif = leftMotor - rightMotor
-    else:
-        dif = rightMotor - leftMotor
+for x in range(20):
+    dif = rightMotor - leftMotor
     print "%s degrees" % getAngle(dif, diameter)
-    leftMotor += 20
+    leftMotor += degreeMove
