45f9fb4231a580b11c638943713384541c5d15c1
Deansenburg <dmurphy1994@hotmail.co.uk>
2015-03-31 10:41:42 +0100

    pos test code

--- lword to dec.py
+++ lword to dec.py
@@ -23,7 +23,7 @@ def getAngle(pos, diameter):
     angle = pos/(diameter*m.pi) * 100 # percentage of perimeter compared to distance moved
     angle *= m.pi/50 #convert percentage of perimter to radian angle
     angle = normaliseAngle(angle)
-    angle *=180/m.pi #convert to degrees
+    #angle *=180/m.pi #convert to degrees
     return angle
 
 def posOnPer(per, angle):
@@ -54,13 +54,31 @@ def targetAngle(lm, rm, diameter, tAngle):
     print "Movements L: %s, R: %s, Angle %s, target %s" % (leftMov, rightMov, curAngle, tAngle)
     return lm, rm
 
+
+def getNewPos(l, r, l2, r2, diameter):
+    x = 0
+    y = 0
+    angle1 = getAngle(l-r, diameter)
+    angle2 = getAngle(l2-r2, diameter)
+    res = 2
+    angleDist = min(abs(angle1-angle2), angle2-angle1)/res
+    dist = (((l2-l) + (r2-r)) / 2) / res
+    for i in range(res+1):
+        x += m.cos(angle1)*dist
+        y += m.sin(angle1)*dist
+        angle1 += angleDist
+        
+    return x, y
 diameter = 1000
-degreeMove = (m.pi * diameter)/360
-leftMotor = lwordToDec([0, 1, 0, 0])
-rightMotor = lwordToDec([255, 255, 255, 0])
+#leftMotor = lwordToDec([0, 1, 100, 0])
+#rightMotor = lwordToDec([255, 255, 255, 0])
 
-print "Start %s" % getAngle(leftMotor-rightMotor, diameter)
-leftMotor, rightMotor = targetAngle(leftMotor, rightMotor, diameter, 90)
-leftMotor, rightMotor = targetAngle(leftMotor, rightMotor, diameter, 180)
+l = 400
+r = 400
+l2 = l + 100
+r2 = r - 100
 
+x, y = getNewPos(l, r, l2, r2, diameter)
+print x
+print y
 
